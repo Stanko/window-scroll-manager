f09a49429484f7bf8714236308f227abf5ad710b
Stanko <stanko.tadic@gmail.com>
2017-11-30 14:57:05 +0100

    Init

---
+++ README.md
@@ -0,0 +1,51 @@
+# Window Scroll Manager
+
+[Demo](https://stanko.github.io/window-scroll-manager)
+
+## What it this?
+
+Listening to window's `onscroll` event is very expensive and ineffective,
+so this library takes different approach on it.
+It is a singleton which sets interaval (60 times per second) when it checks for window scroll position.
+If the scroll position has changed it dispatches custom event (`window-scroll`) with the new position.
+
+## Usage
+
+Get it from [npm](https://www.npmjs.com/package/window-sroll-manager):
+
+```sh
+npm install window-scroll-manager
+```
+
+Import it and use it in your code:
+
+```
+import ScrollManager from 'window-scroll-manager'
+
+const sm = new ScrollManager();
+
+window.addEventListener('window-scroll', function(e) {
+  console.log('Scroll position is: ' + e.detail.scrollPosition;
+});
+```
+
+## Options
+
+`ScrollManager` accepts only one argument `interval` which is the time interval to be used with `setInterval`.
+Default is `16` (~62fps).
+
+Please note that `ScrollManager` is singleton, so `interval` of the first instance will be used.
+
+## API
+
+  * `getScrollPosition` - returns current window scroll position.
+  * `destroy` - destroys the singleton instance and removes interval.
+
+```
+const sm = new ScrollManager();
+
+console.log(sm.getScrollPosition()); // -> 0
+
+sm.destroy();
+sm = null;
+```
---
+++ index.js
@@ -0,0 +1,99 @@
+'use strict';
+
+(function () {
+  var instance = null;
+  var instancesCount = 0;
+
+  var INTERVAL = 16;
+  var EVENT_NAME = 'window-scroll';
+
+  // CustomEvent polyfill
+  if (typeof window !== 'undefined' && typeof window.CustomEvent !== 'function') {
+    var CustomEventPollyfill = function (
+      event,
+      params = { bubbles: false, cancelable: false, detail: undefined }
+    ) {
+      var evt = document.createEvent('CustomEvent');
+      evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+      return evt;
+    };
+
+    CustomEventPollyfill.prototype = window.Event.prototype;
+
+    window.CustomEvent = CustomEventPollyfill;
+  }
+
+  function ScrollManager(userInterval) {
+    var interval = userInterval || INTERVAL;
+
+    instancesCount++;
+
+    if (instance) {
+      return instance;
+    }
+
+    instance = this;
+
+    // Bind handlers
+    this.handleInterval = this.handleInterval.bind(this);
+
+    this.intervalID = setInterval(this.handleInterval, interval);
+  }
+
+  ScrollManager.prototype.removeListener = function () {
+    instancesCount--;
+
+    if (instancesCount === 0) {
+      // Remove and reset interval/animationFrame
+      clearInterval(this.intervalID);
+      this.intervalID = null;
+      // Clear singleton instance
+      instance = null;
+    }
+  };
+
+  ScrollManager.prototype.destroy = function () {
+    instancesCount == 0;
+
+    // Remove and reset interval/animationFrame
+    clearInterval(this.intervalID);
+    this.intervalID = null;
+    // Clear singleton instance
+    instance = null;
+  };
+
+  ScrollManager.prototype.getScrollPosition = function () {
+    // Get scroll position, with IE fallback
+    return window.scrollY || document.documentElement.scrollTop;
+  };
+
+  ScrollManager.prototype.handleInterval = function () {
+    var newScrollPosition = this.getScrollPosition();
+
+    // Fire the event only when scroll position is changed
+    if (newScrollPosition !== this.scrollPosition) {
+      this.scrollPosition = newScrollPosition;
+
+      var event = new CustomEvent(EVENT_NAME, {
+        detail: {
+          scrollPosition: newScrollPosition
+        }
+      });
+
+      // Dispatch the event.
+      window.dispatchEvent(event);
+    }
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    ScrollManager.default = ScrollManager;
+    module.exports = ScrollManager;
+  } else if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
+    // register as 'classnames', consistent with npm package name
+    define('window-scroll-manager', [], function () {
+      return ScrollManager;
+    });
+  } else {
+    window.ScrollManager = ScrollManager;
+  }
+}).call(this);
